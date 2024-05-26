310e4587366edd004b831e4bbd651eee2b61416f
Etheral <etheralgit1@gmail.com>
2024-05-26 20:43:58 +0700

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,148 @@
-import Image from "next/image";
+"use client";
+import React, { useEffect, useState } from "react";
+import { FaXTwitter } from "react-icons/fa6";
+import { LiaTelegramPlane } from "react-icons/lia";
 
 export default function Home() {
+  const [isMuted, setIsMuted] = useState(true);
+
+  useEffect(() => {
+    let videoTimeout: any;
+    videoTimeout = setTimeout(() => {
+      setIsMuted(false);
+    }, 50);
+
+    return () => {
+      clearTimeout(videoTimeout);
+    };
+  }, []);
+
+  useEffect(() => {
+    let zombies: HTMLElement[] = [];
+    let lastMouseMoveTime = 0;
+
+    const getClosestSide = (x: any, y: any) => {
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      const sides = [
+        { x: 0, y },
+        { x: width, y },
+        { x, y: 0 },
+        { x, y: height },
+      ];
+
+      let closestSide = sides[0];
+      let minDistance =
+        Math.abs(x - closestSide.x) + Math.abs(y - closestSide.y);
+
+      sides.forEach((side) => {
+        const distance = Math.abs(x - side.x) + Math.abs(y - side.y);
+        if (distance < minDistance) {
+          closestSide = side;
+          minDistance = distance;
+        }
+      });
+
+      return closestSide;
+    };
+
+    const spawnZombie = (x: any, y: any) => {
+      const zombie = document.createElement("div");
+      zombie.classList.add("zombie-wrapper");
+
+      const zombieImage = new Image();
+      zombieImage.src = "/zombie-icon.png";
+      zombieImage.classList.add("zombie", "animate-spin-slow");
+
+      // Generate a random size between 0.4rem and 2rem
+      const size = Math.random() * (2 - 0.4) + 0.4;
+      zombieImage.style.width = `${size}rem`;
+      zombieImage.style.height = `${size}rem`;
+
+      zombie.style.position = "absolute";
+      zombie.style.left = `${x}px`;
+      zombie.style.top = `${y}px`;
+
+      zombie.appendChild(zombieImage);
+      document.body.appendChild(zombie);
+      zombies.push(zombie);
+
+      const opacity = Math.random() * (1 - 0.3) + 0.2;
+      zombie.style.opacity = opacity.toString();
+
+      const { x: sideX, y: sideY } = getClosestSide(x, y);
+      setTimeout(() => {
+        zombie.style.transition = "transform 4s linear, opacity 2s ease-out";
+        zombie.style.transform = `translate(${sideX - x}px, ${sideY - y}px)`;
+
+        setTimeout(() => {
+          zombie.style.opacity = "0";
+          setTimeout(() => {
+            zombie.remove();
+            zombies = zombies.filter((z) => z !== zombie);
+          }, 2000);
+        }, 8000);
+      }, 1);
+    };
+
+    const handleMouseMove = (e: any) => {
+      const currentTime = Date.now();
+      if (currentTime - lastMouseMoveTime > 80) {
+        lastMouseMoveTime = currentTime;
+        const x = e.clientX;
+        const y = e.clientY;
+        spawnZombie(x, y);
+      }
+    };
+
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+      zombies.forEach((zombie) => zombie.remove());
+    };
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex justify-center items-center w-full h-full flex-col ">
+      <div className="relative max-w-7xl w-full min-h-screen flex justify-center items-center flex-col px-12 py-8 overflow-hidden">
+        <div className="relative md:mt-auto mt-24 overflow-hidden">
+          <video src="/provided-video.mp4" autoPlay loop muted={isMuted} />
+          <div className="absolute bg-gradient-to-b from-black w-full h-24 top-0 inset-x-0" />
+          <div className="absolute bg-gradient-to-t from-black w-full h-24 bottom-0 inset-x-0" />
+          <div className="absolute bg-gradient-to-l from-black h-full w-24 right-0 inset-y-0" />
+          <div className="absolute bg-gradient-to-r from-black h-full w-24 left-0 inset-y-0" />
+        </div>
+
+        <div className="w-full md:mt-auto sm:mt-72 mt-24 flex flex-col gap-4">
+          <div className="border-t border-[#3c3c3c] opacity-40 w-full h-2" />
+          <div className="flex justify-between items-center sm:px-8 px-2 text-[#3c3c3c] z-[100]">
+            <h1 className="sm:text-lg text-sm mr-auto">
+              &copy; All Rights Reserved
+            </h1>
+            <div className="flex items-center gap-4">
+              <a
+                href=""
+                className="bg-[#2a2a2a] sm:p-3 p-2 rounded-md hover:rounded-full duration-300 hover:opacity-80"
+              >
+                <FaXTwitter className="sm:w-6 sm:h-6 w-4 h-4 text-[#a5a592]" />
+              </a>
+              <a
+                href=""
+                className="bg-[#2a2a2a] sm:p-3 p-2 rounded-md hover:rounded-full duration-300 hover:opacity-80"
+              >
+                <LiaTelegramPlane className="sm:w-6 sm:h-6 w-4 h-4 text-[#a5a592]" />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <div className="absolute bg-gradient-to-b from-black w-full h-72 top-0 inset-x-0 z-[50]" />
+      <div className="absolute bg-gradient-to-t from-black w-full h-24 bottom-0 inset-x-0 z-[50]" />
+      <div className="absolute bg-gradient-to-l from-black h-full w-36 right-0 inset-y-0 z-[50]" />
+      <div className="absolute bg-gradient-to-r from-black h-full w-36 left-0 inset-y-0 z-[50]" />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+// tailwind.config.js
+module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    // Tambahkan path ke file lainnya jika perlu
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      animation: {
+        "spin-slow": "spin 2s linear infinite",
+      },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
       },
     },
   },
   plugins: [],
 };
-export default config;
